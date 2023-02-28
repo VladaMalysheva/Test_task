0f3173af5a24b2f33ab8988aa00331f51dff5bb4
VladaMalysheva <ik02.vlada.m@gmail.com>
2023-03-01 00:25:24 +0300

    Connected database

--- src/main/java/com/example/test_task/config/TestTaskConfig.java
+++ src/main/java/com/example/test_task/config/TestTaskConfig.java
@@ -1,28 +1,23 @@
 package com.example.test_task.config;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mchange.v2.c3p0.DriverManagerDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
-import java.util.logging.Logger;
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example.test_task")
-//@PropertySource("classpath:persistence-mysql.properties")
 public class TestTaskConfig {
 
-    private final Logger log = Logger.getLogger(getClass().getName());
 
-    @Autowired
-    Environment env;
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -31,5 +26,15 @@ public class TestTaskConfig {
         return viewResolver;
     }
 
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test_task");
+        dataSource.setUser("root");
+        dataSource.setPassword("admin");
+
+        return dataSource;
+    }
 
 }
